31d8aa773706a09606d0de499998924ecb2a48b4
Evgeny Derkachev <derkachev987@mail.ru>
2021-06-20 18:14:03 +0300

    изменил цвет заголовка

---
+++ src/Components/Content/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Content = () => {
+    return (
+        <div className="body">
+            <h1>Это место для контента!</h1>
+        </div>
+    )
+}
+
+export default Content;
