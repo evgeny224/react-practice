3531b62d6207d333fe0e8631e5a8327b14a0ef3d
Evgeny Derkachev <derkachev987@mail.ru>
2021-06-21 17:54:25 +0300

    сделал шаблон главной страницы

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-
+import React from "react";
 import './App.css';
-import Header from "./Components/Header";
-import Content from './Components/Content';
-import Footer from './Components/Footer';
+import logo from "./images/wolf_logo.png";
+import sand from "./images/sand.jpg"
+
 
 const App = () => {
   return (
     
-        <div className="App">
-          <Header />
-          <Content />
-          <Footer />
+        <div className="app-wrapper">
+          <header className="header">
+            <div>
+            < img src={logo} alt="logo" />
+            </div>
+          </header>
+          <nav className="nav">
+            <div>Profile</div>
+            <div>Message</div>
+            <div>News</div>
+            <div>Music</div>
+            <div>Settings</div>
+          </nav>
+          <div className="content">
+            <div>
+              <img src={sand} alt="island" />
+            </div>
+            <div>ava+description</div>
+            <div>
+              My posts
+              <div>New post</div>
+              <div>
+                <div>post 1</div>
+                <div>post 2</div>
+              </div>
+            </div>
+          </div>
         </div>
   );
 }
